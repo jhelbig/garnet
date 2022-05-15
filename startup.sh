f33f4ee68f1505c7ea9a51df418f58a32fe49883
Jake Helbig <jake@jakehelbig.com>
2022-05-15 12:12:13 -0500

    re-org of kill scripts to ensure that all garnet processes are dead before killing redis.

--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ ! -z "$(pidof redis-server)" ]; then
-  kill -9 "$(pidof redis-server)"
-fi
-
 while [ ! -z "$(ps -ef | grep garnet | egrep -v "grep")" ]
 do
   echo "killing garnet processes"
@@ -11,7 +7,15 @@ do
   sleep 5
 done
 
+if [ ! -z "$(pidof redis-server)" ]; then
+  echo "killing redis server"
+  kill -9 "$(pidof redis-server)"
+  sleep 5
+fi
+
+
 if [ "$GARNET_REDIS_HOST" == "0.0.0.0" ]; then
+  echo "starting garnet redis server on $GARNET_REDIS_HOST"
   envsubst < /usr/share/project/redis.conf.template > /etc/redis/redis.conf
   redis-server /etc/redis/redis.conf --daemonize yes
 else
