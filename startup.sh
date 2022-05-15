32e7980b1c3f73e7d996fe21244fbe58155922ad
Jake Helbig <jake@jakehelbig.com>
2022-05-15 01:09:02 -0500

    upgraded to self-cleanup and init on both dev and non-dev envs.

--- startup.sh
+++ startup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ ! -z "$(pidof redis-server)" ]; then
+  kill -9 "$(pidof redis-server)"
+fi
+
+while [ ! -z "$(ps -ef | grep garnet | egrep -v "grep")" ]
+do
+  echo "killing garnet processes"
+  ps -ef | grep garnet | egrep -v "grep" | awk '{ system("kill -9 " $2) }'
+  sleep 5
+done
+
 if [ "$GARNET_REDIS_HOST" == "0.0.0.0" ]; then
   envsubst < /usr/share/project/redis.conf.template > /etc/redis/redis.conf
   redis-server /etc/redis/redis.conf --daemonize yes
@@ -7,8 +18,17 @@ else
   echo "Skipping Redis setup, please make sure that connections to $GARNET_REDIS_HOST can be made from this host!"
 fi
 
-garnet -s downloader &
-sleep 1
-garnet -s usher &
-sleep 1
-garnet -s web
+echo "Starting $APP_ENV"
+if [ "$APP_ENV" == "dev" ]; then
+  crystal run src/garnet.cr -- -s downloader &
+  sleep 1
+  crystal run src/garnet.cr -- -s usher &
+  sleep 1
+  crystal run src/garnet.cr -- -s web
+else
+  garnet -s downloader &
+  sleep 1
+  garnet -s usher &
+  sleep 1
+  garnet -s web
+fi
